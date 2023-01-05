f52aea43a6b69b21edcbd96197af4a5c88429551
parvyyy <112224844+parvyyy@users.noreply.github.com>
2023-01-05 17:03:03 +1100

    Created the empty board

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+
+const boardSize = 8;
+
+// Creating the board
+let gameBoard = new Array(boardSize);
+for (let i = 0; i < gameBoard.length; i++) {
+  gameBoard[i] = new Array(boardSize);
+}
+
+console.log('Hello World');
